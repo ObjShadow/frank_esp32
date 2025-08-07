588eeb28f8be96860e4bc983da8dc1c126f58a49
lsk_china <colaleo225@gmail.com>
2025-08-07 19:04:50 +0800

    Update protobuf files

--- main/network.h
+++ main/network.h
@@ -5,4 +5,21 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
+#include "proto/packet.pb-c.h"
+
+struct network_provider {
+	int id;
+  	int (*connect_to_server)(void);
+    int (*send)(void *data, size_t size);
+	int (*receive)(void *data, size_t size);
+};
+
+static void (*on_text_packet)(char* text);
+static void (*on_audio_packet)(void* data, size_t size);
+
+void start_network_task(struct network_provider *provider);
+void send_voice_to_server(void *data, size_t size);
+void send_command_to_server(Commands command);
+void set_one
+
 #endif //NETWORK_H
