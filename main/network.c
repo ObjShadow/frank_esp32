d6e339b70cba358fb933136a9438826ce8782655
lsk_china <colaleo225@gmail.com>
2025-08-07 19:30:52 +0800

    network module

--- main/network.c
+++ main/network.c
@@ -2,4 +2,19 @@
 // Created by lsk on 6/27/25.
 //
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
 #include "network.h"
+
+// Message queues to implement the communication between main task and network task
+QueueHandle_t queue_send_to_network;
+QueueHandle_t queue_send_to_main;
+
+// Network task handle
+TaskHandle_t task_handle;
+
+// Callbacks
+void (*on_text_packet)(char* text);
+void (*on_audio_packet)(void* data, size_t size);
--- main/network.h
+++ main/network.h
@@ -11,15 +11,13 @@ struct network_provider {
 	int id;
   	int (*connect_to_server)(void);
     int (*send)(void *data, size_t size);
-	int (*receive)(void *data, size_t size);
+	int (*receive)(void *data, size_t *size);
 };
 
-static void (*on_text_packet)(char* text);
-static void (*on_audio_packet)(void* data, size_t size);
-
 void start_network_task(struct network_provider *provider);
-void send_voice_to_server(void *data, size_t size);
+void send_voice_to_server(void *data, size_t *size);
 void send_command_to_server(Commands command);
-void set_one
+void set_on_text_packet(void (*on_text)(char *text));
+void set_on_audio_packet(void (*on_audio)(void* data, size_t size));
 
 #endif //NETWORK_H
