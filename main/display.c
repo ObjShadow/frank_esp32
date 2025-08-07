29397582e5ee6f2997362c0719702f1f079ea7e9
lsk_china <colaleo225@gmail.com>
2025-08-07 23:12:48 +0800

    introduce display dependencies, and some basic structures

--- main/display.c
+++ main/display.c
@@ -1,3 +1,11 @@
 //
 // Created by lsk on 6/27/25.
 //
+
+#include "display.h"
+
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_ssd1306.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_lvgl_port.h"
+#include "lvgl.h"
--- main/display.h
+++ main/display.h
@@ -5,4 +5,18 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+/*
+ * Well I have to admit that I did a bad job when creating module display and hardware, they should be either merged,
+ * or be separated into more modules, but, well, I'm lazy to fix this issue, and since the code will work no matter how
+ * I arrange these modules, and the readability won't be influenced too much with enough comments, so just let it go ;)
+ *
+ * Hope one day I or someone else would fix it...
+ *
+ * Module display: code for OLED display, and for WS2812.
+ * Module hardware: code for I2S devices (microphone and amplifier), and buttons.
+ * These modules should only provide a wrap for GPIO and bus operations. Their logic should be implemented in main.c .
+ */
+
+
+
 #endif //DISPLAY_H
